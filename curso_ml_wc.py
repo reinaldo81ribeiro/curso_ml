706d961e421181ee1c03bff02b56431791a8ef7e
reinaldo_ribeiro <reinaldo.ribeiro@mega.com.br>
2017-08-29 09:39:27 -0300

    removendo as keys

--- curso_ml_wc.py
+++ curso_ml_wc.py
@@ -3,13 +3,13 @@ import tweepy
 import time
 import csv
 
-cons_key = "7hsppVq26xRNndnGg2tPhMObQ"
-cons_secret = "rtlfgDxQCCI4VJ72sZQalryNhnnSMZcUzflX9F3xIjWn6PgGS5"
-tok_key = "FFTKLYY7K33DVFEWd8Xg6N9hadhnST2"
-tok_secret = "OfPtsrrDJ7U4JLPejn6i0VCIAYfcOXHDNG431WP9U9lkK"
+cons_key = ""
+cons_secret = ""
+tok_key = ""
+tok_secret = ""
 
 auth = tweepy.OAuthHandler(cons_key, cons_secret)
-auth.set_access_token(tok_key, tok_secret)
+#auth.set_access_token(tok_key, tok_secret)
 
 api = tweepy.API(auth)
 
@@ -23,6 +23,7 @@ statuses = tweepy.Cursor(api.search, q="#brazil", sin="2017-08-27", until="2017-
 while True:
     try:
         status = statuses.next()
+        print(80*"=")
         print(status)
         exit()
     except tweepy.TweepError:
@@ -30,4 +31,5 @@ while True:
         time.sleep(60*15)
         continue
     except StopIteration:
+        print("acabou")
         break
