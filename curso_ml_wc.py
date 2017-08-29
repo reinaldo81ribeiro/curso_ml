c6cee69574462ccc161dd43ca6c97ca86de1865d
reinaldo_ribeiro <reinaldo.ribeiro@mega.com.br>
2017-08-29 09:11:33 -0300

    segundo commit

--- curso_ml_wc.py
+++ curso_ml_wc.py
@@ -1,11 +1,10 @@
-#   coding: utf-8
 
 import tweepy
 import time
 import csv
 
-cons_key = "TMOWMdgN7U2LuygzQonoJHsrc"
-cons_secret = "m65XdfcY0w5cXqCdVg3aG9dX7mWJh79aW2NpdQkfn9v4mgkpxH"
+cons_key = "7hsppVq26xRNndnGg2tPhMObQ"
+cons_secret = "rtlfgDxQCCI4VJ72sZQalryNhnnSMZcUzflX9F3xIjWn6PgGS5"
 tok_key = "FFTKLYY7K33DVFEWd8Xg6N9hadhnST2"
 tok_secret = "OfPtsrrDJ7U4JLPejn6i0VCIAYfcOXHDNG431WP9U9lkK"
 
@@ -13,8 +12,6 @@ auth = tweepy.OAuthHandler(cons_key, cons_secret)
 auth.set_access_token(tok_key, tok_secret)
 
 api = tweepy.API(auth)
-print(api.proxy)
-
 
 arq = csv.writer(open("base_teste.csv", "w"))
 arq2 = open("base_teste.json", "w")
@@ -25,10 +22,8 @@ statuses = tweepy.Cursor(api.search, q="#brazil", sin="2017-08-27", until="2017-
 
 while True:
     try:
-        for status in statuses:
-            print(status)
-            exit()
-
+        status = statuses.next()
+        print(status)
         exit()
     except tweepy.TweepError:
         print("agurde 15 minutos")    
